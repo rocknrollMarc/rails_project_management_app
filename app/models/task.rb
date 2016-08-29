370d6c5818afed5a5fa746dd103de427b4624e97
Marc Bluemner <marc.bluemner@gmail.com>
2016-08-29 14:39:58 +0200

    added active record

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,32 @@
-class Task
-	attr_accessor :size, :completed
+#---
+# Excerpted from "Rails 4 Test Prescriptions",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/nrtest2 for more book information.
+#---
+class Task < ActiveRecord::Base
 
-	def initialize(options = {})
-		@completed = options[:completed]
-		@size = options[:size]
-	end
+  belongs_to :project
 
-	def mark_completed
-		@completed = true
-	end
+  def mark_completed(date = nil)
+    self.completed_at = (date || Time.current)
+  end
 
-	def complete?
-		@completed
-	end
+  def complete?
+    completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > Project.velocity_length_in_days.days.ago
+  end
+
+
+  def points_toward_velocity
+    if part_of_velocity? then size else 0 end
+  end
+
 end
+
