2f65a5090f562ff11c15d0424c9cda664a5f8b97
Marc Bluemner <marc.bluemner@gmail.com>
2016-09-01 23:43:59 +0200

    roles

--- spec/features/user_and_role_spec.rb
+++ spec/features/user_and_role_spec.rb
@@ -1,25 +1,43 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'with users and roles' do
-	
-	def log_in_as(user)
-		visit new_user_session_path
-		fill_in('user_email', :with => user.email)
-		fill_in('user_password', :with => user.password)
-		click_button('Log in')
-	end
+describe "with users and roles" do
 
-	let(:user) { User.create(email: 'test@example.com', password: 'password') }
+  def log_in_as(user) 
+    visit new_user_session_path
+    fill_in("user_email", :with => user.email)
+    fill_in("user_password", :with => user.password)
+    click_button("Log in")
+  end
 
-	it 'allows a logged-in user to view the project index page' do
-		log_in_as(user)
-		visit(projects_path)
-		expect(current_path).to eq(projects_path)
-	end
+  let(:user) { User.create!(email: "test2@example.com", password: "password") }
 
-	it 'does not allow user to see the project page if not logged in' do
-		visit(projects_path)
-		expect(current_path).to eq(new_user_session_path)
-	end
-	
+  it "allows a logged-in user to view the project index page" do
+    log_in_as(user)
+    visit(projects_path)
+    expect(current_path).to eq(projects_path)
+  end
+
+  it "does not allow user to see the project page if not logged in" do
+    visit(projects_path)
+    expect(current_path).to eq(new_user_session_path)
+  end
+
+  describe "roles" do
+    let(:project) { Project.create(name: "Project Gutenberg") }
+
+    it "allows a user who is part of a project to see that project" do
+      project.roles.create(user: user)
+      log_in_as(user)
+      visit(project_path(project))
+      expect(current_path).to eq(project_path(project))
+    end
+
+    it "does not allow a user who is not part of a project to see that project" do
+      log_in_as(user)
+      visit(project_path(project))
+      expect(current_path).not_to eq(project_path(project))
+    end
+  end
+
 end
+
