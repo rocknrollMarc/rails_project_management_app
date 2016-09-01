9bb31c4fa6bc816568646d2684650117ebe0f404
Marc Bluemner <marc.bluemner@gmail.com>
2016-09-01 17:54:55 +0200

    task_completed_email mailer

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+
+	def update
+		@task = Task.find(params[:id])
+		completed = params[:task].delete(:completed)
+		params[:task][:completed_at] = Time.current if completed
+		if @task.update_attributes(params[:task].permit(:size, :completed_at))
+			TaskMailer.task_completed_email(@task).deliver
+		if completed
+			redirect_to @task, notice: "'project was successfully updated.'"
+		else
+			render action: 'edit'
+		end
+	end
+
+	def show
+		@task = Task.find(params[:id])
+	end
 end
