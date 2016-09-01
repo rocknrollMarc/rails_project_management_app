515b0e385bb62623216d9f45cd951c1fb5cd0edb
Marc Bluemner <marc.bluemner@gmail.com>
2016-09-01 16:20:35 +0200

    name_with_status helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,6 @@
 module ProjectsHelper
 	def name_with_status(project)
-		content_tag(:span, project.name, class: 'on_schedule')
+		dom_class = project.on_schedule? ? 'on_schedule' : 'behind_schedule'
+		content_tag(:span, project.name, class: dom_class)
 	end
 end
